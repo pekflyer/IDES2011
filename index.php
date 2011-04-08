89f12c0108b18c4c2874a862b00f871699aa0ce4
Henri Kuschkowitz <cubeandcore@gmail.com>
2011-04-08 18:11:24 -0400

    added group display

--- index.php
+++ index.php
@@ -5,16 +5,11 @@
 		 $db_control = new dataBase();
 	     ?>
 
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<!--<link rel="stylesheet" href="http://static.jquery.com/ui/css/demo-docs-theme/ui.theme.css" type="text/css" media="all"/>
-<link rel="stylesheet" href="css/jquery.hrzAccordion.examples.css" type="text/css" media="all"/>
-<link rel="stylesheet" href="css/jquery.hrzAccordion.defaults.css" type="text/css" media="all"/>-->
 <link rel="stylesheet" href="css/main.css" type="text/css" media="all"/>
-<!--<link rel="stylesheet" href="css/style.css" type="text/css" media="all"/>-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js" type="text/javascript"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js" type="text/javascript"></script>
 <script src="scripts/jquery.quicksand.js" type="text/javascript"></script>
@@ -24,110 +19,159 @@
 
 <body>
 	<script>
-		(function($) {
-		  $.fn.sorted = function(customOptions) {
-			var options = {
-			  reversed: false,
-			  by: function(a) { return a.text(); }
+		(function($) 
+		{
+			$.fn.sorted = function(customOptions) 
+		  	{
+				var options = 
+				{
+			  		reversed: false,
+			  		by: function(a) { return a.text(); 
+				}
 			};
+			
 			$.extend(options, customOptions);
 			$data = $(this);
 			arr = $data.get();
-			arr.sort(function(a, b) {
-			  var valA = options.by($(a));
-			  var valB = options.by($(b));
-			  if (options.reversed) {
-				return (valA < valB) ? 1 : (valA > valB) ? -1 : 0;				
-			  } else {		
-				return (valA < valB) ? -1 : (valA > valB) ? 1 : 0;	
-			  }
+			
+			arr.sort(function(a, b) 
+			{
+				var valA = options.by($(a));
+			  	var valB = options.by($(b));
+			  	if (options.reversed) 
+			  	{
+					return (valA < valB) ? 1 : (valA > valB) ? -1 : 0;				
+			  	} 
+			  	else 
+			  	{		
+					return (valA < valB) ? -1 : (valA > valB) ? 1 : 0;	
+			  	}
 			});
+			
 			return $(arr);
-		  };
+		};
 		})(jQuery);
 		
 		// DOMContentLoaded
-		$(function() {
-		
-		  // bind radiobuttons in the form
-		  var $filterGroup = $('#filter input[name="group"]');
-		  var $filterSort = $('#filter input[name="sort"]');
-		
-		  // get the first collection
-		  var $students = $('#students');
-		
-		  // clone students to get a second collection
-		  var $data = $students.clone();
-		
-		  // attempt to call Quicksand on every form change
-		  $filterGroup.add($filterSort).change(function(e) {
-			if ($($filterGroup+':checked').val() == 'all') {
-			  var $filteredData = $data.find('li');
-			} else {
-			  var $filteredData = $data.find('li[data-type=' + $($filterGroup+":checked").val() + ']');
-			}
-		
-			// sorted by first name
-			if ($('#filter input[name="sort"]:checked').val() == "first") {
-				//if sorted by first name
-			  var $sortedData = $filteredData.sorted({
-				by: function(v) {
-					return $(v).find('strong').text().toLowerCase();
-				}
-			  });
-			}
-			// sorted by first name
-			else if ($('#filter input[name="sort"]:checked').val() == "last") {
-			  // if sorted by last name
-			  var $sortedData = $filteredData.sorted({
-				by: function(v) {
-					return $(v).find('span[data-type="last"]').text().toLowerCase();
-				}
-			  });
-			}  
-		
-			// finally, call quicksand
-			$students.quicksand($sortedData, {
-			  duration: 800,
-			  easing: 'easeInOutQuad'
-			});
-		
-		  });
-		
-		});
-		
-		
-		jQuery(document).ready(function() 
-		{
-			lastBlock = $("#home");
-			maxWidth = 730;
-			minWidth = 30;	
+		$(function() 
+		{	
+			// bind radiobuttons in the form
+			var $filterGroup = $('#filter input[name="group"]');
+			var $filterSort = $('#filter input[name="sort"]');
 			
-			//$(lastBlock).animate({width: maxWidth+"px"}, { queue:false, duration:400 });
+			// get the first collection
+			var $students = $('#students');
 			
-			//main navigation
-			$("ul.mainContent li.mainNav").mousedown
-			(
-				function()
+			// get projects container
+			var $transitionType;
+			var $projects = $('#projects_info');
+			$projects.hide(); // hide at start
+			
+			
+			// clone students to get a second collection
+			var $data = $students.clone();
+			
+			// attempt to call Quicksand on every form change
+			$filterGroup.add($filterSort).change(function(e) 
+			{
+				if ($($filterGroup+':checked').val() == 'all') 
 				{
-					$(lastBlock).animate({width: minWidth+"px"}, { queue:false, duration:400});
-					
-					$(this).animate({width: maxWidth+"px"}, { queue:false, duration:400});
-					lastBlock = this;
+					$transitionType = "Out";
+					var $filteredData = $data.find('li');
+				} 
+				else 
+				{
+					$transitionType = "In";
+					//$projects.fadeIn("slow");
+				  	
+				  	var $filteredData = $data.find('li[data-type=' + $($filterGroup+":checked").val() + ']');
 				}
-			);
 			
-			$('.sorting_box #groups :radio').focus(updateSelectedStyle);
-       		$('.sorting_box #groups :radio').blur(updateSelectedStyle);
-        	$('.sorting_box #groups :radio').change(updateSelectedStyle);
-    	
-	    	function updateSelectedStyle() 
-	    	{
-		        $('.sorting_box #groups :radio').parent().removeClass('focused');//.next().removeClass('focused');
-		        $('.sorting_box #groups :radio:checked').parent().addClass('focused');//.next().addClass('focused');
-		    }
+				// sorted by first name
+				if ($('#filter input[name="sort"]:checked').val() == "first") 
+				{
+					var $sortedData = $filteredData.sorted(
+					{
+						by: function(v) 
+						{
+							return $(v).find('strong').text().toLowerCase();
+						}
+					});
+				}
+				// sorted by last name
+				else if ($('#filter input[name="sort"]:checked').val() == "last") 
+				{
+					// if sorted by last name
+				  	var $sortedData = $filteredData.sorted(
+				  	{
+						by: function(v) 
+						{
+							return $(v).find('div[data-type="last"]').text().toLowerCase();
+						}
+					});
+			}  
+	
+			function reOrganize()
+			{
+				if($transitionType == "In") 
+				{
+					//$projects.slideIn();
+					$projects.slideDown("slow", runQuicksand());
+				}
+				else 
+				{
+					//$projects.slideOut();
+					$projects.slideUp("slow", runQuicksand());
+				}
+			}
 			
+			function runQuicksand()
+			{
+				setTimeout(function()
+				{
+					$students.quicksand($sortedData, 
+					{
+						duration: 600,
+						easing: 'easeInOutQuad'
+					})
+				}, 601);
+			}
+			
+			reOrganize();
 		});
+	});
+	
+	
+	jQuery(document).ready(function() 
+	{
+		lastBlock = $("#home");
+		maxWidth = 740;
+		minWidth = 30;	
+		
+		//main navigation
+		$("ul.mainContent li.mainNav").mousedown
+		(
+			function()
+			{
+				$(lastBlock).animate({width: minWidth+"px"}, { queue:false, duration:400});
+				
+				$(this).animate({width: maxWidth+"px"}, { queue:false, duration:400});
+				lastBlock = this;
+			}
+		);
+		
+		//highlight the selected view option
+		$('.sorting_box #groups :radio').focus(updateSelectedStyle);
+   		$('.sorting_box #groups :radio').blur(updateSelectedStyle);
+    	$('.sorting_box #groups :radio').change(updateSelectedStyle);
+		
+		function updateSelectedStyle() 
+    	{
+    		
+	        $('.sorting_box #groups :radio').parent().removeClass('focused');//.next().removeClass('focused');
+	        $('.sorting_box #groups :radio:checked').parent().addClass('focused');//.next().addClass('focused');
+	    }
+	});
 		
 	</script>	
 	<div class="full">
@@ -137,17 +181,16 @@
 			  <div class="nav" id="nav_home"> 
 		  	  <div class="content">
 			        <div id="theme">
-                    	
-                      <div id="map">
-						<img src="images/home/map.png" />
-                        <hr />
-                        <p>Industrial designers improve upon existing products and develop new products that 
-                        	enhance our ever-changing lifestyles. From concept to production, Carleton University 
-                            Industrial Design students continually define, refine, and redefine problems and solutions.</p>
-                           <p>The School of Industrial Design welcomes you to visit the 33rd Annual Graduation 
-                           Exhibition, to explore student projects completed within the Bachelor of 
-                           Industrial Design program.</p>
-                           <p>Parking available in Lot P2. Free weekend parking. Free admission.</p>
+                    	<div id="map">
+                        	<p>
+                            	APRIL 16TH-19TH, 10AM-5MP<br />
+                                UNIVERSITY CENTRE GALLERIA
+                            </p>
+                        	<img src="images/home/map.png" />
+                            <p>
+                            	PARKING AVAILABLE IN LOT 2<br />
+								FREE PARKING ON WEEKENDS
+                            </p>
                       </div>
 		            <img src="images/home/exhibitlogo_green.png" /></div>
 			    </div>
@@ -156,9 +199,17 @@
 			<li class="mainNav" id="projects" title="Projects">
 			  <div class="nav" id="nav_projects">
 			    <div class="content">
-			      <ul id="students" class="image-grid">
-				
-                
+			    	<div id="projects_info">
+			    		<div id="proj_group">
+			    			<h3 id="proj_name"></h3>
+			    			<h1 id="proj_client"></h1>
+			    		</div>
+			    		<div id="project_nav">
+			    			<div id="prevProj"></div>
+			    			<div id="nextProj"></div>
+			    		</div>
+			    	</div>			    
+			      <ul id="students" class="image-grid">         
                 	<?php
 					 	$query =  $db_control->query_getStudents();
 						$i=1;
@@ -166,11 +217,10 @@
 							echo '<li data-id="id-'.$i.'" data-type="'.$row["grp_name"].'">';
 							echo '<img src="./images/students/'.$row["stu_fname"].'_'.$row["std_lname"].'.jpg" />';
 							echo '<div class="StuName"><strong>'. $row["stu_fname"] .'</strong> ';
-							echo '<span data-type="last">'.$row["std_lname"].'</span></div>';
+							echo '<div data-type="last">'.$row["std_lname"].'</div></div>';
 							echo '</li>';
 							$i++;
 						}
-	
 					?>
 			
 			      </ul>
@@ -191,7 +241,8 @@
 			            <fieldset id="fullnames">
 			              <legend>Sort by Name</legend>
 			              <label><input type="radio" name="sort" value="first" checked="checked">First</label>
-			              <label><input type="radio" name="sort" value="last">Last</label>      
+			              <label><input type="radio" name="sort" value="last">Last</label>
+			              <label><input type="radio" name="sort" value="last">All</label>      
 			            </fieldset>
 					</div>
 			    </div>
@@ -200,26 +251,29 @@
 			<li class="mainNav" id="alumni" title="Alumni">
 			  <div class="nav" id="nav_alumni">
 			  	<div class="content">
-                       	<div id="#AlumniTitle">
-                        	<p>Alumni Night</p>
-                            <p>APRIL 16TH, 2011, 5–8PM, UNIVERSITY CENTRE GALLERIA</p>
-                        </div>
-                        <div id="AlumniMap">
-                        	<p>The School of Industrial Design and the Carleton University Alumni Association, Industrial Design Chapter, invite you to the annual Alumni Night, on April 16th, 2011 at 5PM, to honour the contributions of Jacques Ostiguy to the School of Industrial Design. Featuring presentations by Floyd Pushelberg and many others.</p>
-                            <p>Please register at <a href="alumni.carleton.ca/events">alumni.carleton.ca/events</a></p>
-                            <hr />
-                            <img src="images/Alumni Night/Map.png" />
-                        </div>
-                        <div id="AlumniPics">
-                        
-                        </div>
+                	Alumni Night<br />APRIL 16TH, 2011, 5–8PM, UNIVERSITY CENTRE GALLERIA<br/><br/>
+                    	<table width="700" border="0">
+						  <tr>
+  						 	 <td><center>
+                             	<img src="images/Ostiguy.jpg"  /><br /><br /><br />
+                                <img src="images/CSeal.png" width="120" height="120" alt="Cseal" /></center><br /><br />
+                                Complimentary refreshments.<br/>
+                                Free parking available in Lot P2.</td>
+ 						 	  <td width="490"><br /><br />
+                              The School of Industrial Design and the Carleton University Alumni Association, Industrial Design 	Chapter, invite you to the annual Alumni Night, on April 16th, 2011 at 5PM, to honour the contributions of Jacques Ostiguy to the School of Industrial Design. Featuring presentations by Floyd Pushelberg and many others.<br/>
+Please register at alumni.carleton.ca/events<br/>
+	<img src="images/bar.jpg" alt="" width="490" height="3" /><br/>
+<img src="images/map.jpg" width="490" height="237" alt="map" border='1' /></td>
+						 </tr>
+						</table>
+                       	
 			    </div>	  
 			  </div>
 			</li>
 			<li class="mainNav" id="sponsor" title="Sponsors">
 			  <div class="nav" id="nav_sponsors">
 			  		<div class="content">
-                    	Sponsors + Thanks<br /><br />The School of Industrial Design and the 2010-11 Bachelor of Industrial Design graduating class would like to sincerely thank this year’s collaborators and sponsors for their contributions and support.
+                    	Sponsors + Thanks<br /><br />The School of Industrial Design and the 2010-11 Bachelor of Industrial Design graduating class would like to sincerely thank this year’s collaborators and sponsors for their contributions and support.          	
 			    	</div>	  
 			  	</div>  
 			</li>
